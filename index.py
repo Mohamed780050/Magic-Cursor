b15fbc9a7b15b83da5f22691fe456f8377948d7f
MoBakr99 <mbakry32017@gmail.com>
2025-09-21 16:33:11 +0300

    Enabled Web Cam

--- index.py
+++ index.py
@@ -1,2 +1,22 @@
-name = input("What is your name?\n")
-age = input("How old you are?\n")
+import cv2
+print(cv2.__version__)
+
+
+# Webcam dimensions
+wCam, hCam = 1280, 720
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+while True :
+    success, img = cap.read()  # Capture frame from webcam
+    if not success:
+        break
+    cv2.imshow("Img", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
