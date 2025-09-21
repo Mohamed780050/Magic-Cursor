6cfa5514f453bf95d019ccb1509210649ed152c0
Mohamed780050 <mohamed39omer@gmail.com>
2025-09-21 16:15:48 +0300

    testing

--- index.py
+++ index.py
@@ -1,16 +1,2 @@
-import os
-import sys
-
-def start_screen_session():
-    session_name = "my_python_session"
-    command = f"screen -dmS {session_name} python3 {os.path.abspath(__file__)}"
-    os.system(command)
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "child":
-        # Your main script logic here
-        print("Executing Python code in screen session...")
-        input("Hello")
-        # Add your code here
-    else:
-        start_screen_session()
+name = input("What is your name?\n")
+age = input("How old you are?\n")
